a8f5d04db80e3b76c2a5335f6e74ad2db0c26e21
Matvei Karibdzhanov <matveichrk2@gamil.com>
2025-05-02 23:39:03 +0900

    Fei

--- run_all.sh
+++ run_all.sh
@@ -1,14 +1,25 @@
-./reconstruct_belle1_mc_hadron_y5s.sh evtgen-nonbsbs 5 &
-./reconstruct_belle1_mc_hadron_y5s.sh evtgen-nonbsbs 4 &
-./reconstruct_belle1_mc_hadron_y5s.sh evtgen-nonbsbs 3 &
-./reconstruct_belle1_mc_hadron_y5s.sh evtgen-nonbsbs 2 &
-./reconstruct_belle1_mc_hadron_y5s.sh evtgen-nonbsbs 1 &
-./reconstruct_belle1_mc_hadron_y5s.sh evtgen-nonbsbs 0 &
+#!/bin/bash
 
+for i in {0..5}
+do
+    ./reconstruct_belle1_mc_hadron_y5s.sh evtgen-nonbsbs $i &
+    wait
+done
 
-./reconstruct_belle1_mc_hadron_y5s.sh evtgen-uds 5 &
-./reconstruct_belle1_mc_hadron_y5s.sh evtgen-uds 4 &
-./reconstruct_belle1_mc_hadron_y5s.sh evtgen-uds 3 &
-./reconstruct_belle1_mc_hadron_y5s.sh evtgen-uds 2 &
-./reconstruct_belle1_mc_hadron_y5s.sh evtgen-uds 1 &
-./reconstruct_belle1_mc_hadron_y5s.sh evtgen-uds 0 &
+for i in {0..5}
+do
+    ./reconstruct_belle1_mc_hadron_y5s.sh evtgen-uds $i &
+    wait
+done
+
+for i in {0..5}
+do 
+    ./reconstruct_belle1_mc_hadron_y5s.sh evtgen-charm $i &
+    wait
+done
+
+for i in {0..5}
+do
+    ./reconstruct_belle1_mc_hadron_y5s.sh evtgen-bsbs $i &
+    wait
+done
