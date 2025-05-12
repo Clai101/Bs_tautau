adea864dd777c11bfe362c5246eca1665512add3
Matvei Karibdzhanov <matveichrk2@gamil.com>
2025-05-12 13:42:58 +0900

    cut

---
+++ cut.cc
@@ -0,0 +1,89 @@
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <TMinuit.h>
+#include <TCanvas.h>
+#include <TChain.h>
+#include <TTree.h>
+#include <TMath.h>
+#include <TH1F.h>
+#include <Math/SpecFuncMathCore.h>
+
+/*
+
+TChain *pchn = new TChain("BS")
+pchn->Add("BsBs_bsbs.root")
+TH1F *a1 = new TH1F("a1","",400,-0.1,0.1)
+TH1F *a2 = new TH1F("a2","",400,-0.1,0.1)
+TH1F *h01 = new TH1F("h01","",400,-0.1,0.1)
+
+
+*/
+
+
+void invisible_uds_tree() {
+
+
+
+double missedE, M0, p0, recM2, N_KL, idec0, idec1, totalEnergyMC, E_gamma_in_ROE, N_tracks_in_ROE, Bs_lik, is0, lost_nu_0, lost_gamma_0, lost_pi_0, lost_K_0, Miss_id_0, lost_nu_1, lost_gamma_1, lost_pi_1, lost_K_1, Miss_id_1;
+int N_tracks_in_ROE, N_KS,N_KL2;
+    
+pchn->SetBranchAddress("missedE", &missedE);
+pchn->SetBranchAddress("M0", &M0);
+pchn->SetBranchAddress("p0", &p0);
+pchn->SetBranchAddress("recM2", &recM2);
+pchn->SetBranchAddress("N_KL", &N_KL);
+pchn->SetBranchAddress("idec0", &idec0);
+pchn->SetBranchAddress("idec1", &idec1);
+pchn->SetBranchAddress("totalEnergyMC", &totalEnergyMC);
+pchn->SetBranchAddress("E_gamma_in_ROE", &E_gamma_in_ROE);
+pchn->SetBranchAddress("N_tracks_in_ROE", &N_tracks_in_ROE);
+pchn->SetBranchAddress("Bs_lik", &Bs_lik);
+pchn->SetBranchAddress("is0", &is0);
+pchn->SetBranchAddress("lost_nu_0", &lost_nu_0);
+pchn->SetBranchAddress("lost_gamma_0", &lost_gamma_0);
+pchn->SetBranchAddress("lost_pi_0", &lost_pi_0);
+pchn->SetBranchAddress("lost_K_0", &lost_K_0);
+pchn->SetBranchAddress("Miss_id_0", &Miss_id_0);
+pchn->SetBranchAddress("lost_nu_1", &lost_nu_1);
+pchn->SetBranchAddress("lost_gamma_1", &lost_gamma_1);
+pchn->SetBranchAddress("lost_pi_1", &lost_pi_1);
+pchn->SetBranchAddress("lost_K_1", &lost_K_1);
+pchn->SetBranchAddress("Miss_id_1", &Miss_id_1);
+
+TFile *f = new TFile("invisible_uds_wo_tracks.root", "recreate");
+TTree *pchn1 = new TTree("Y5S", "Simple tree");
+
+pchn1->Branch("missedE", &missedE);
+pchn1->Branch("M0", &M0);
+pchn1->Branch("p0", &p0);
+pchn1->Branch("recM2", &recM2);
+pchn1->Branch("N_KL", &N_KL);
+pchn1->Branch("idec0", &idec0);
+pchn1->Branch("idec1", &idec1);
+pchn1->Branch("totalEnergyMC", &totalEnergyMC);
+pchn1->Branch("E_gamma_in_ROE", &E_gamma_in_ROE);
+pchn1->Branch("N_tracks_in_ROE", &N_tracks_in_ROE);
+pchn1->Branch("Bs_lik", &Bs_lik);
+pchn1->Branch("is0", &is0);
+pchn1->Branch("lost_nu_0", &lost_nu_0);
+pchn1->Branch("lost_gamma_0", &lost_gamma_0);
+pchn1->Branch("lost_pi_0", &lost_pi_0);
+pchn1->Branch("lost_K_0", &lost_K_0);
+pchn1->Branch("Miss_id_0", &Miss_id_0);
+pchn1->Branch("lost_nu_1", &lost_nu_1);
+pchn1->Branch("lost_gamma_1", &lost_gamma_1);
+pchn1->Branch("lost_pi_1", &lost_pi_1);
+pchn1->Branch("lost_K_1", &lost_K_1);
+pchn1->Branch("Miss_id_1", &Miss_id_1);
+
+
+int nentries = pchn->GetEntries();    
+for (int i = 0; i < nentries; ++i) {
+    pchn->GetEntry(i);
+    if (sigProb > 0.0001 && abs(pcm-0.47)<0.1)
+        pchn1->Fill();
+}
+pchn1->Write();
+}
