910957ef1b471b3c04db79fe59c2add7c931b93a
Matvei Karibdzhanov <matveichrk2@gamil.com>
2025-06-28 22:31:53 +0900

    sig_ch

--- run_all.sh
+++ run_all.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
+#!/bin/bash
 
 for i in {0..5}
 do
     ./reconstruct_belle1_mc_hadron_y5s.sh evtgen-nonbsbs $i s &
-    wait
 done
+wait
 
 for i in {0..5}
 do
     ./reconstruct_belle1_mc_hadron_y5s.sh evtgen-bsbs $i s &
-    wait
 done
+wait
 
 for i in {0..5}
 do
     ./reconstruct_belle1_mc_hadron_y5s.sh evtgen-uds $i l &
-    wait
 done
+wait
 
 for i in {0..5}
 do 
     ./reconstruct_belle1_mc_hadron_y5s.sh evtgen-charm $i l &
-    wait
 done
+wait
