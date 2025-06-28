f8f8cf1f649f8ff897ae0781ad791bbf9735494c
Matvei Karibdzhanov <matveichrk2@gamil.com>
2025-06-28 22:17:21 +0900

    sig_ch

--- test_sig.sh
+++ test_sig.sh
@@ -2,7 +2,6 @@
 
 file="/home/belle2/matrk/Extend/gsim_out/mdst/evtgen_exp_67_bszerotautau-206.mdst"
 
-cp Bs_2tau_mc_FEI.py ${WORKDIR}
 
 OUTPUT_FILE="mc_sig.root"
 echo "basf2 -l error Bs_2tau_mc_FEI.py \"${file}\" ${OUTPUT_FILE} Sig_mc &> mc.log" > job_script
