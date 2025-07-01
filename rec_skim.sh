2a19357752b627d23699490c003bac4a1354e95b
Matvei Karibdzhanov <matveichrk2@gamil.com>
2025-07-01 21:36:11 +0900

    skim

---
+++ rec_skim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+EXPERIMENT=$1
+RUN_START=$2
+RUN_END=$3
+TY=$4
+DATA_TYPE=$5
+STREAM=$6
+TIME=$7
+
+WORKDIR=/group/belle2/users2022/matrk/Gen_mc/mc_${EXPERIMENT}_${RUN_START}_${RUN_END}_${TY}_${DATA_TYPE}_${STREAM}
+WORkCODE="Bs_my_skim.py" 
+absolute_path="$(pwd)/${WORkCODE}"
+OUTPUT_FILE="mc"
+
+URL="http://bweb3/montecarlo.php?ex=${EXPERIMENT}&rs=${RUN_START}&re=${RUN_END}&ty=${TY}&dt=${DATA_TYPE}&bl=caseB&dv=zfserv&st=${STREAM}"
+
+rm -rf ${WORKDIR}
+mkdir -p ${WORKDIR}
+cd ${WORKDIR}
+
+echo "basf2 -l error $absolute_path \"${URL}\" ${OUTPUT_FILE}.root ${TY}_${STREAM} &> ${OUTPUT_FILE}.log" > job_script
+chmod 755 job_script
+bsub -q ${TIME} ./job_script
+_prod00024786_task10020000
