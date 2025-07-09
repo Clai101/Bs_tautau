9db6d28f1b52f355df19b9a32ffd36b073b7a29b
Matvei Karibdzhanov <matveichrk2@gamil.com>
2025-07-09 09:02:05 +0900

    Gen_MC_re

--- test_sig.sh
+++ test_sig.sh
@@ -4,7 +4,7 @@ file="/home/belle2/matrk/Extend/gsim_out/mdst/bszerotautau/evtgen_exp_69_bszerot
 
 
 OUTPUT_FILE="mc_sig.root"
-echo "basf2 -l error mdst_root.py \"${file}\" ${OUTPUT_FILE} Sig_mc &> mc.log" > job_script
+echo "basf2 -l error Bs_2tau_mc_FEI.py \"${file}\" ${OUTPUT_FILE} Sig_mc &> mc.log" > job_script
 chmod 755 job_script
 ./job_script
 rm job_script
